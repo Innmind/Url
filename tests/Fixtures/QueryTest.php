1d6f528ca473d784dc200d0f7819efadf57ac005
Baptiste Langlade <baptouuuu@gmail.com>
2020-06-14 12:39:23 +0200

    fix fixtures tests

--- tests/Fixtures/QueryTest.php
+++ tests/Fixtures/QueryTest.php
@@ -6,7 +6,10 @@ namespace Tests\Innmind\Url\Fixtures;
 use Fixtures\Innmind\Url\Query;
 use Innmind\Url\Query as Model;
 use PHPUnit\Framework\TestCase;
-use Innmind\BlackBox\Set;
+use Innmind\BlackBox\{
+    Set,
+    Random\RandomInt,
+};
 
 class QueryTest extends TestCase
 {
@@ -16,7 +19,7 @@ class QueryTest extends TestCase
 
         $this->assertInstanceOf(Set::class, $set);
 
-        foreach ($set->values() as $value) {
+        foreach ($set->values(new RandomInt) as $value) {
             $this->assertInstanceOf(Set\Value::class, $value);
             $this->assertTrue($value->isImmutable());
             $this->assertInstanceOf(Model::class, $value->unwrap());
